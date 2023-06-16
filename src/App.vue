7e1bcbbe96039b891952f25ba2ee6218efc6069b
Isabella Fernanda Campos Emiliano <isabellaemiliano14@gmail.com>
2023-06-16 14:09:23 -0300

    Adicionando arquivos iniciais do projeto

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <Cardapio />
+  </div>
+</template>
+
+<script>
+import Cardapio from "./components/Cardapio.vue";
+
+export default {
+  name: "App",
+  components: {
+    Cardapio,
+  },
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+  font-family: "Open Sans", sans-serif;
+}
+</style>
---
+++ src/components/Cardapio.vue
@@ -0,0 +1,151 @@
+<template>
+  <div class="cardapio">
+    <section class="items">
+      <h4>Selecione os produtos</h4>
+
+      <div v-for="produto in produtos" :key="produto.id" class="produto">
+        <div class="photo">
+          <img :src="produto.photo" />
+        </div>
+        <div class="description">
+          <span class="name">{{ produto.name }}</span>
+          <span class="price">$ {{ produto.price }}</span>
+          <div class="quantity-area">
+            <template v-if="produto.active">
+              <button
+                :disabled="produto.quantity <= 1"
+                @click.stop="produto.quantity--"
+              >
+                -
+              </button>
+              <span class="quantity">{{ produto.quantity }}</span>
+              <button @click.stop="produto.quantity++">+</button>
+            </template>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "CardapioPrincipal",
+  data() {
+    return {
+      produtos: {},
+    };
+  },
+  methods: {
+    getProducts() {
+      axios
+        .get("http://localhost:3000/Produtos")
+        .then((res) => {
+          this.produtos = res.data;
+          console.log(res);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+  created() {
+    this.getProducts();
+  },
+};
+</script>
+<style scoped>
+h4 {
+  text-align: center;
+  margin-top: 0;
+  width: 100%;
+}
+.cardapio {
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  flex-wrap: wrap;
+  padding-top: 20px;
+}
+
+.items {
+  display: flex;
+  flex-wrap: wrap;
+  border: 1px solid lightgrey;
+  padding: 20px;
+  max-width: 500px;
+  min-width: 300px;
+  justify-content: center;
+}
+
+.produto {
+  border: 1px solid lightgrey;
+  margin: 6px;
+  flex: 0 0 calc(33.333% - 24px);
+  cursor: pointer;
+  text-align: center;
+}
+
+.selected {
+  border: 2px solid rgb(29, 134, 233);
+}
+
+.photo img {
+  max-width: 90px;
+}
+
+.description {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  font-size: 11px;
+  line-height: 15px;
+}
+
+.price {
+  font-weight: bold;
+  margin: 4px auto;
+}
+
+section.items .description .quantity-area {
+  margin: 8px auto;
+  height: 14px;
+}
+
+.quantity-area button {
+  width: 16px;
+  height: 16px;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+}
+
+.quantity {
+  font-weight: bold;
+  margin: 0 4px;
+}
+
+.summary {
+  background-color: rgb(245, 245, 245);
+  padding: 20px;
+  min-height: 200px;
+  min-width: 200px;
+  text-align: center;
+}
+
+.summary table {
+  width: 100%;
+  padding-top: 12px;
+  font-size: 11px;
+  margin: auto;
+}
+
+.summary table tbody tr:last-of-type th {
+  border-top: 1px solid black;
+  padding-top: 4px;
+}
+</style>
